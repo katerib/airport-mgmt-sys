eedfa43a09412be37234788bf8d8034159c4dbe4
Raj-Lee-Code <rajan.lee97@gmail.com>
2023-05-24 18:00:14 -0400

    Updated app.js to have routes pointing to handlebar files and added read functions to terminals table/page

--- app.js
+++ app.js
@@ -1,25 +1,71 @@
-// App.js
-
 /*
     SETUP
 */
-var express = require('express');
-var app     = express();            // instantiate an express object to interact with the server
-PORT        = 9126;
 
-app.use(express.static('public'));
+// Express
+const express = require('express');
+const app = express();
+PORT = 9336;
+
+// Database
+const db = require('./database/db-connector');
+
+// Handlebars
+const { engine } = require('express-handlebars');
+const exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
 /*
     ROUTES
 */
-app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
-    {
-        res.sendFile(__dirname + '/index.html');
-    });
+
+//gets home page
+app.get('/', function(req, res)
+    {  
+        {res.render('index');}                                                    
+    });                                                         
+
+//gets Terminals page
+app.get('/terminals', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Terminals;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('terminals', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query
+
+//gets Gates page
+app.get('/gates', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Gates;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('gates', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query
+
+//gets Flights page
+app.get('/flights', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Flights;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('flights', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query
+
+
+
+
 
 /*
     LISTENER
 */
-app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+app.listen(PORT, function(){
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
